0b1a6b61bfc6e3c8842f91afb18e98c51688f083
Vasil Vangelovski <vvangelovski@gmail.com>
2009-12-21 03:42:42 +0100

    added testproject

---
+++ testproject/manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#pwd = os.path.dirname(__file__)
+#parent = os.path.join(pwd, '..')
+#sys.path.append(parent)
+
+from django.core.management import execute_manager
+try:
+    import settings # Assumed to be in the same directory.
+except ImportError:
+    import sys
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    execute_manager(settings)
