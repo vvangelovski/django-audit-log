6d5c2d88a998439afb293774b982a6c77b1153c2
Vasil Vangelovski <vvangelovski@gmail.com>
2010-10-06 15:35:34 +0200

    bumped version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ ez_setup.use_setuptools()
 from setuptools import setup
 setup(
     name = 'django-audit-log',
-    version = '0.1',
+    version = '0.2',
     packages = ['audit_log', 'audit_log.models'],
     author = 'Vasil Vangelovski',
     author_email = 'vvangelovski@gmail.com',
@@ -11,7 +11,7 @@ setup(
     description = 'Audit trail for django models',
     long_description = 'This app automatically tracks changes made to models over time as well as which user made the changes.',
     url = 'http://code.google.com/p/django-audit-log/',
-    download_url = 'http://django-audit-log.googlecode.com/files/django-audit-log.0.1.tar.gz',
+    download_url = 'http://code.google.com/p/django-audit-log/downloads/list',
     include_package_data = True,
     
     classifiers=[
