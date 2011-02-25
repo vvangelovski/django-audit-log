81ddbc9a93d8d95c8db47979b3a90a7f231051b3
Vasil Vangelovski <vvangelovski@gmail.com>
2011-02-25 12:14:07 +0100

    added more tests that utilize the complete request-response cycle

--- testproject/store/tests.py
+++ testproject/store/tests.py
@@ -32,12 +32,16 @@ class LogEntryMetaOptionsTest(TestCase):
         self.failUnlessEqual(WarehouseEntry.audit_log.model._meta.db_table, "%sauditlogentry"%WarehouseEntry._meta.db_table)
 
 
-class SimpleLoggingTest(TestCase):
+class LoggingTest(TestCase):
     
-    def test_logging_single(self):
-        _setup_admin()
+    def setup_client(self):
         c = Client()
         c.login(username = "admin", password = "admin")
+        return c
+    
+    def test_logging_insert_update(self):
+        _setup_admin()
+        c = self.setup_client()
         c.post('/admin/store/productcategory/add/', {'name': 'Test Category', 'description': 'Test description'})
         self.failUnlessEqual(ProductCategory.objects.all().count(), 1)
         category = ProductCategory.objects.all()[0]
@@ -45,16 +49,30 @@ class SimpleLoggingTest(TestCase):
         self.failUnlessEqual(category.audit_log.all()[0].description, category.description)
         self.failUnlessEqual(category.audit_log.all()[0].action_type, "I")
         self.failUnlessEqual(category.audit_log.all()[0].action_user.username, "admin")
-        c.post('/admin/store/productcategory/%s/'%category.pk, {'name': 'Test Category new name', 'description': 'Test description'})
+    
+        c.post('/admin/store/productcategory/%s/'%'Test Category', {'name': 'Test Category new name', 'description': 'Test description'})
         category = ProductCategory.objects.get(pk = "Test Category new name")
         self.failUnlessEqual(category.audit_log.all().count(), 1)
         self.failUnlessEqual(category.audit_log.all()[0].name, "Test Category new name")
         self.failUnlessEqual(category.audit_log.all()[0].action_type, "I")
-        c.post('/admin/store/productcategory/%s/'%category.pk, {'name': 'Test Category new name', 'description': 'Test modified description'})
+        
+        c.post('/admin/store/productcategory/%s/'%'Test Category new name', {'name': 'Test Category new name', 
+                                                                            'description': 'Test modified description'})
         category = ProductCategory.objects.get(pk = "Test Category new name")
         self.failUnlessEqual(category.audit_log.all().count(), 2)
         self.failUnlessEqual(category.audit_log.all()[0].description, "Test modified description")
         self.failUnlessEqual(category.audit_log.all()[0].action_type, "U")
+    
+    def test_logging_delete(self):
+        _setup_admin()
+        c = self.setup_client()
+        c.post('/admin/store/productcategory/add/', {'name': 'Test', 'description': 'Test description'})
+        self.failUnlessEqual(ProductCategory.objects.all().count(), 1)
+        c.post('/admin/store/productcategory/Test/delete/', {'post': 'yes'})
+        self.failUnlessEqual(ProductCategory.objects.all().count(), 0)
+        self.failUnlessEqual(ProductCategory.audit_log.all().count(), 2)
+        self.failUnlessEqual(ProductCategory.audit_log.all()[0].action_type, "D")        
+        
         
     def test_logging_inherited(self):
         _setup_admin()
@@ -65,4 +83,5 @@ class SimpleLoggingTest(TestCase):
         self.failUnlessEqual(widget.audit_log.all()[0].name, 'Test name')
         self.failUnlessEqual(hasattr(widget.audit_log.all()[0], 'special_power'), True)
         self.failUnlessEqual(widget.audit_log.all()[0].special_power, "Testpower")
+        
         
