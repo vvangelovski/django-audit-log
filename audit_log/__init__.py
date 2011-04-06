3ebf0719a358488369b455e36eb0b7bc7e26aa74
Vasil Vangelovski <vvangelovski@gmail.com>
2011-04-06 13:11:11 +0200

    fixed typos in README. Moved documentation from readme. Added versioning information in the __init__.py of the package. Tidy setup.py, made it point to new project home

---
+++ audit_log/__init__.py
@@ -0,0 +1,6 @@
+VERSION = (0, 2, 2, 'final')
+
+if VERSION[-1] != "final": # pragma: no cover
+    __version__ = '.'.join(map(str, VERSION))
+else: # pragma: no cover
+    __version__ = '.'.join(map(str, VERSION[:-1]))
--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+
+from audit_log import VERSION, __version__
+
+if VERSION[-1] == 'final':
+    STATUS = ['Development Status :: 5 - Production/Stable']
+elif 'beta' in VERSION[-1]:
+    STATUS = ['Development Status :: 4 - Beta']
+else:
+    STATUS = ['Development Status :: 3 - Alpha']
     
 setup(
     name = 'django-audit-log',
-    version = '0.2.1',
-    packages = ['audit_log', 'audit_log.models'],
+    version = __version__,
+    packages = find_packages(exclude = ['testproject']),
     author = 'Vasil Vangelovski',
     author_email = 'vvangelovski@gmail.com',
     license = 'New BSD License (http://www.opensource.org/licenses/bsd-license.php)',
     description = 'Audit trail for django models',
-    long_description = 'This app automatically tracks changes made to models over time as well as which user made the changes.',
-    url = 'http://code.google.com/p/django-audit-log/',
-    download_url = 'http://code.google.com/p/django-audit-log/downloads/list',
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url = 'https://github.com/Atomidata/django-audit-log',
+    download_url = 'https://github.com/Atomidata/django-audit-log/downloads',
     include_package_data = True,
     
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Plugins',
+    classifiers = STATUS + [
+       'Environment :: Plugins',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
