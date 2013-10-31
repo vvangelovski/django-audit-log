c4b88a14205820c741c257dbcd1899f0620bcde4
Brian Wilson <BrianWilson@g1group.co.uk>
2013-10-31 09:41:01 +0000

    Use AUTH_USER_MODEL setting instead of auth.User for log auditor

--- audit_log/models/fields.py
+++ audit_log/models/fields.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from audit_log import registration
 
 class LastUserField(models.ForeignKey):
@@ -7,10 +7,10 @@ class LastUserField(models.ForeignKey):
     A field that keeps the last user that saved an instance
     of a model. None will be the value for AnonymousUser.
     """
-    
-    def __init__(self, to = User, null = True,  **kwargs):
+
+    def __init__(self, to = settings.AUTH_USER_MODEL, null = True,  **kwargs):
         super(LastUserField, self).__init__(to = to, null = null, **kwargs)
-    
+
     def contribute_to_class(self, cls, name):
         super(LastUserField, self).contribute_to_class(cls, name)
         registry = registration.FieldRegistry(self.__class__)
