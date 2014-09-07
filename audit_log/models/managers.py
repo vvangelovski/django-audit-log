068cbbb714eba859e83c5d3d6948ced310f94076
Erik Romijn <eromijn@solidlinks.nl>
2014-09-07 20:43:12 +0200

    Prevented creation of permissions by default for audit log models.

--- audit_log/models/managers.py
+++ audit_log/models/managers.py
@@ -183,6 +183,7 @@ class AuditLog(object):
         return {
             'ordering' : ('-action_date',),
             'app_label' : model._meta.app_label,
+            'default_permissions': (),
         }
     
     def create_log_entry_model(self, model):
