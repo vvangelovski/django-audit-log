47590b81b584495093d27cfbf749982dd99bc68a
Vasil Vangelovski <vvangelovski@gmail.com>
2015-09-27 19:34:48 +0200

    fix for #28 + docs

--- audit_log/middleware.py
+++ audit_log/middleware.py
@@ -25,14 +25,19 @@ def _enable_audit_log_managers(instance):
 
 
 class JWTAuthMiddleware(object):
+    """
+    Convenience middleware for users of django-rest-framework-jwt.
+    Fixes issue https://github.com/GetBlimp/django-rest-framework-jwt/issues/45
+    """
 
 
     def get_user_jwt(self, request):
         from rest_framework.request import Request
+        from rest_framework.exceptions import AuthenticationFailed
         from django.utils.functional import SimpleLazyObject
         from django.contrib.auth.middleware import get_user
         from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-        
+
         user = get_user(request)
         if user.is_authenticated():
             return user
@@ -40,13 +45,15 @@ class JWTAuthMiddleware(object):
             user_jwt = JSONWebTokenAuthentication().authenticate(Request(request))
             if user_jwt is not None:
                 return user_jwt[0]
-        except:
+        except AuthenticationFailed:
             pass
         return user
 
     def process_request(self, request):
         from django.utils.functional import SimpleLazyObject
-        assert hasattr(request, 'session'), "The Django authentication middleware requires session middleware to be installed. Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."
+        assert hasattr(request, 'session'),\
+        """The Django authentication middleware requires session middleware to be installed.
+         Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."""
 
         request.user = SimpleLazyObject(lambda: self.get_user_jwt(request))
 
@@ -54,6 +61,9 @@ class JWTAuthMiddleware(object):
 
 class UserLoggingMiddleware(object):
     def process_request(self, request):
+        assert hasattr(request, 'session'),\
+        """The Django authentication middleware requires session middleware to be installed.
+         Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."""
         if settings.DISABLE_AUDIT_LOG:
             return
         if not request.method in ('GET', 'HEAD', 'OPTIONS', 'TRACE'):
--- docs/install.rst
+++ docs/install.rst
@@ -15,13 +15,34 @@ The package audit_log doesn't need to be in your ``INSTALLED_APPS``. The only th
 to modify in your ``settings.py`` is add ``audit_log.middleware.UserLoggingMiddleware`` to
 the ``MIDDLEWARE_CLASSES`` tupple::
 
-    
+
     MIDDLEWARE_CLASSES = (
-         'django.middleware.common.CommonMiddleware',
          'django.contrib.sessions.middleware.SessionMiddleware',
+         'django.middleware.common.CommonMiddleware',
+         'django.middleware.csrf.CsrfViewMiddleware',
          'django.contrib.auth.middleware.AuthenticationMiddleware',
+         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+         'django.contrib.messages.middleware.MessageMiddleware',
+         'django.middleware.clickjacking.XFrameOptionsMiddleware',
+         'audit_log.middleware.JWTAuthMiddleware',
          'audit_log.middleware.UserLoggingMiddleware',
     )
 
 
+For users of ``django-rest-framework-jwt`` you should also include a special middleware
+that fixes a compatibility problem with that library::
 
+    MIDDLEWARE_CLASSES = (
+      'django.contrib.sessions.middleware.SessionMiddleware',
+      'django.middleware.common.CommonMiddleware',
+      'django.middleware.csrf.CsrfViewMiddleware',
+      'django.contrib.auth.middleware.AuthenticationMiddleware',
+      'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+      'django.contrib.messages.middleware.MessageMiddleware',
+      'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'audit_log.middleware.JWTAuthMiddleware',
+      'audit_log.middleware.UserLoggingMiddleware',
+    )
+
+Note that in that case ``rest_framework_jwt.authentication.JSONWebTokenAuthentication``
+should be at the top of ``DEFAULT_AUTHENTICATION_CLASSES``.
